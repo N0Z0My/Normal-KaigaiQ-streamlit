075346f408769098e02c9ec45cc0043237d37670
N0Z0My <131774194+N0Z0My@users.noreply.github.com>
2025-03-12 03:20:22 +0000

    last

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ def init_logger():
 def load_data():
     """データの読み込み"""
     try:
-        df = pd.read_excel('kaigai_part15-30.xlsx', sheet_name='sheet1', index_col=0)
+        df = pd.read_excel('kaigai_part1-15.xlsx', sheet_name='sheet1', index_col=0)
         return df
     except Exception as e:
         st.error("データの読み込みに失敗しました。")
